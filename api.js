80bbbf888602a652e6ff2a17dd9c22f62463cc9d
Ganesh Sundar C <77ganesh.nitt@gmail.com>
2020-11-15 11:10:14 +0530

    start server if env!=test

--- api.js
+++ api.js
@@ -5,16 +5,21 @@ const { NODE_ENV } = require('./utils/constants')
 const log = require('./utils/logger')
 const app = new Koa()
 
-async function startServer () {
+async function initServer () {
   await require('./loaders/deps')()
   await require('./loaders/koa')(app)
 
-  if (NODE_ENV !== 'test') {
-    app.listen(config.appPort, () => {
-      log.info({ component: 'server', message: 'started', port: config.appPort })
-    })
-  }
   return app
 }
 
-module.exports = startServer
+async function startServer () {
+  if (NODE_ENV !== 'test') {
+    const server = await initServer()
+    server.listen(config.appPort, () => {
+      log.info({ component: 'server', message: 'server started', port: config.appPort })
+    })
+  }
+}
+startServer()
+
+module.exports = initServer
